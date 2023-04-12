58f62493cbad6d90af1c25e166e5492051664834
Antonio Neto <antonioneto.mec@gmail.com>
2023-04-12 10:04:01 -0300

    Initial commit

---
+++ src/main/java/com/aasn/yourfoodAPI/YourfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.aasn.yourfoodAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aasn/yourfoodAPI/YourfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aasn.yourfoodAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
